7da53caa4721c326806ad8690cb2a91ff3a514e9
Thuan Nguyen <thuan.nguyendinh89@gmail.com>
2024-01-22 19:28:58 +0700

    [BaiTap2] Tinh tien dien

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Khoi tao Git 'baiTapJSBuoi5'
 # Bai Tap 1: Quan Ly Tuyen Sinh
+# Bai Tap 2: Tinh Tien Dien
--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@
  * Outputs:
  * ketQua, tongDiem
  */
-function tinhKQTuyenSinh() {
+function mainTinhKQTuyenSinh() {
     let tongDiem = 0;
     let diemChuan = document.getElementById('diemChuan').value;
     let diemMon1 = document.getElementById('diemMon1').value;
@@ -51,8 +51,8 @@ function tinhKQTuyenSinh() {
         `;
     }
 }
-// Goi ham tinhKQTuyenSinh khi click button 'btnTinhKQTuyenSinh'
-document.getElementById('btnTinhKQTuyenSinh').onclick = tinhKQTuyenSinh;
+// Goi ham mainTinhKQTuyenSinh khi click button 'btnTinhKQTuyenSinh'
+document.getElementById('btnTinhKQTuyenSinh').onclick = mainTinhKQTuyenSinh;
 
 /**
  * Kiem tra diem nhap vao
@@ -66,4 +66,81 @@ function kiemTraDiem(diem) {
     }
 
     return true;
+}
+
+
+
+
+
+/**
+ * Bai 2 : Tinh Tien Dien
+ * 
+ * Inputs:
+ * tenKH, soKw
+ * 
+ * Process:
+ * Function:
+ *   B1: Lấy giá trị từ form
+ *   B2: Kiểm tra so Kw
+ *   B3: Lập công thức tính tienDien
+ * 
+ *   B6: thông báo kết quả
+ * 
+ * Outputs:
+ * tienDien
+ */
+// Bảng Kw điện
+const KW_50_DAU = 500;
+const KW_50_KE = 650;
+const KW_100_KE = 850;
+const KW_150_KE = 1100;
+const KW_CON_LAI = 1300;
+
+function mainTinhTienDien() {
+    let tienDien = 0;
+    let tenKH = document.getElementById('tenKH').value;
+    let soKw = document.getElementById('soKw').value;
+
+    // Kiem tra ten KH
+    if (tenKH === '') {
+        alert("Vui lòng nhập tên KH");
+    } else if (soKw == '' || soKw < 0) {
+        alert("Vui lòng nhập số Kw");
+    } else {
+        // Tinh tiền điện
+        tienDien =  tinhTienDienTheoKW(soKw);
+
+        // Su dung ${} cua ES6 de binding du lieu
+        let txtResult = document.getElementById('txtResultTinhTienDien');
+        txtResult.innerHTML = `<br /> Tên KH : ${tenKH} 
+            <br /> Số tiền điện : ${tienDien.toLocaleString('vi-VN', {style:"currency", currency:"VND"})}
+        `;
+    }
+}
+// Goi ham mainTinhTienDien khi click button 'btnTinhTienDien'
+document.getElementById('btnTinhTienDien').onclick = mainTinhTienDien;
+
+// Ham tinh tien theo KW
+function tinhTienDienTheoKW(soKw) {
+    let tongTien = 0;
+    if (0 < soKw && soKw <= 50) {
+        console.log("Bậc 1");
+        tongTien = soKw * KW_50_DAU;
+    } else if (50 < soKw && soKw <= 100) {
+        console.log("Bậc 2");
+        tongTien = 50 * KW_50_DAU + (soKw - 50) * KW_50_KE;
+    } else if (100 < soKw && soKw <= 200) {
+        console.log("Bậc 3");
+        tongTien = (50 * KW_50_DAU) + (50 * KW_50_KE) + (soKw - 100) * KW_100_KE;
+    } else if (200 < soKw && soKw <= 350 ) {
+        console.log("Bậc 4");
+        tongTien = (50 * KW_50_DAU) + (50 * KW_50_KE) + (100 * KW_100_KE) + (soKw - 200) * KW_150_KE;
+    } else if (soKw > 350 ) {
+        console.log("Bậc 5");
+        tongTien = (50 * KW_50_DAU) + (50 * KW_50_KE) + (100 * KW_100_KE) + (150 * KW_150_KE) + (soKw - 350) * KW_CON_LAI;
+    } else {
+        alert("Hãy nhập lại Kw");
+    }
+
+    return tongTien;
 }
